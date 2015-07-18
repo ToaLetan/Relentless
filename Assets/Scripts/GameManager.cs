0d3fb0e55eddb8ce380b7d77fdfcfec0c276be6a
Tyler Norman <letan881@hotmail.com>
2015-07-18 17:02:07 -0400

    GameJolt Scoreboard API, Splash Screen
    Implemented GameJolt login and scoreboard submission. Scores aren't
    submitted if users aren't logged on. Added a simple splash screen.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,16 @@ using GameJolt;
 public class GameManager : MonoBehaviour 
 {
     public enum GameState { Splash, Running, Paused, Over };
-    private GameState currentGameState = GameState.Running;
+    private GameState currentGameState = GameState.Splash;
 
-    InputManager gameInput = null;
+    private InputManager gameInput = null;
+    private EnemySpawnManager enemySpawner = null;
+
+    private GameObject splashScreen = null;
+
+    private string playerName = "Player";
+
+    private int waveReached = 0;
 
     public GameState CurrentGameState
     { get { return currentGameState; } }
@@ -16,7 +23,30 @@ public class GameManager : MonoBehaviour
 	void Start () 
     {
         gameInput = InputManager.Instance;
-	}
+
+        enemySpawner = gameObject.GetComponent<EnemySpawnManager>();
+
+        splashScreen = GameObject.Find("Main Camera").transform.FindChild("SplashScreen").gameObject;
+
+        //GameJolt.UI.Manager.Instance.ShowSignIn();
+
+        GameJolt.UI.Manager.Instance.ShowSignIn((bool success) => 
+        {
+            if (success)
+            {
+                playerName = GameJolt.API.Manager.Instance.CurrentUser.Name;
+                Debug.Log("The user: " + playerName + " signed in!");
+
+                StartGame();
+            }
+            else
+            {
+                Debug.Log("The user failed to signed in or closed the window :(");
+
+                StartGame();
+            }
+            });
+	    }
 	
 	// Update is called once per frame
 	void Update () 
@@ -28,5 +58,23 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         currentGameState = GameState.Over;
+
+        //If the user has signed in, submit the high score.
+        waveReached = enemySpawner.CurrentWave;
+
+        string scoreboardText = "Wave reached: " + waveReached;
+
+        GameJolt.API.Scores.Add(waveReached, scoreboardText, 0, "", (bool success) => 
+        {
+            Debug.Log(string.Format("Score Add {0}.", success ? "Successful" : "Failed"));
+        });
+    }
+
+    public void StartGame()
+    {
+        //Hide the splash screen, enable objects movement.
+        splashScreen.GetComponent<SpriteRenderer>().enabled = false;
+
+        currentGameState = GameState.Running;
     }
 }
